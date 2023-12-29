2a8711e82c980140d5cdcd8aabf909c9c757667e
Aleksi Backman <bddubbb@gmail.com>
2023-12-29 10:52:50 +0200

    refactor: improve naming and structure
    Tested with a pre-downloaded SDK tar package.
    
    * add selection logic for installing runtime dependencies
    * use /tmp for storing intermediate files
    * add more progress prints
    * add new TODO comment to end of main()

--- scripts/install_vulkan_sdk.sh
+++ scripts/install_vulkan_sdk.sh
@@ -18,6 +18,11 @@ set -e
 readonly vulkansdk_web='vulkansdk-linux-x86_64-1.3.261.1'
 readonly vulkansdk_tar="${vulkansdk_web}.tar.xz"
 
+error_exit() {
+	echo "${1}"
+	exit 1
+}
+
 download_sdk_tar_package() {
 	echo "Dropping the SDK tar package to host"
 	if [ ! -f ./"${vulkansdk_tar}" ] ; then
@@ -32,27 +37,12 @@ verify_sdk_tar_package() {
 	echo "Verifying SDK tar package checksum"
 
 	if [ "${vulkan_tar_checksum_from_web}" != "${vulkan_tar_checksum_local}" ] ; then
-		echo "Downloaded vulkan sdk package has unexpected checksum: ${vulkan_tar_checksum_local}"
-		exit 1
+		error_exit "Downloaded vulkan sdk package has unexpected checksum: ${vulkan_tar_checksum_local}"
 	fi
 }
 
-# TODO
-# * Temporarily store downloaded resources to a common place.
-#     * Add config.sh which tells the common place for resources.
-# * Write in functions
-# * Perform the download in 'deps_install_workarea'
-main() {
-	local -r vulkansdk_destination="${HOME}/my/tools"
-	local -r vulkansdk_local_renamed="${vulkansdk_web}"
-	vulkansdk_local=''
-
-	# TODO consider a more descriptive name
-	if [ ! -d vulkan ] ; then
-		mkdir vulkan
-	fi
-
-	pushd vulkan
+install_runtime_dependencies_ubuntu_2204() {
+	# Ubuntu 22.04:	qtbase5-dev libxcb-xinput0 libxcb-xinerama0
 
 	echo "Updating currently installed packages via apt"
 	sudo apt update
@@ -60,11 +50,71 @@ main() {
 
 	echo "Installing pre-requisites via apt"
 	sudo apt -y install qtbase5-dev libxcb-xinput0 libxcb-xinerama0
+}
+
+install_runtime_dependencies_ubuntu_2004() {
+	# Ubuntu 20.04:	qt5-default libxcb-xinput0 libxcb-xinerama0
+
+	echo "Function not yet implemented: install_runtime_dependencies_ubuntu_2004()"
+}
+
+install_runtime_dependencies_fedora() {
+	# Fedora:	qt xinput libXinerama
+
+	echo "Function not yet implemented: install_runtime_dependencies_fedora()"
+}
+
+install_runtime_dependencies_arch() {
+	# Arch:		qt5-base libxcb libxinerama
+
+	echo "Function not yet implemented: install_runtime_dependencies_arch()"
+}
+
+install_runtime_dependencies_slackware() {
+	echo "Function not yet implemented: install_runtime_dependencies_slackware()"
+}
+
+install_runtime_dependencies() {
+	local -r distro_name="$(grep '^NAME=' /etc/os-release | cut -d = -f 2)"
+
+	echo "Installing runtime dependencies"
+
+	# if [ # Ubuntu 22.04 # ] ; then
+	# 	install_runtime_dependencies_ubuntu_2204
+	# fi
+
+	if [ "Slackware" == "${distro_name}" ] ; then
+		install_runtime_dependencies_slackware
+		return
+	fi
+
+	error_exit "Unsupported Linux distribution: ${distro_name}"
+}
+
+# TODO
+# * Temporarily store downloaded resources to a common place.
+#     * Add config.sh which tells the common place for resources.
+main() {
+	local -r vulkansdk_workarea="/tmp/vulkansdk_workarea"
+	local -r vulkansdk_destination="${HOME}/my/tools"
+	local -r vulkansdk_extracted_renamed="${vulkansdk_web}"
+	local vulkansdk_extracted=''
+
+	echo "Starting Vulkan SDK install"
+
+	# TODO consider a more descriptive name
+	if [ ! -d "${vulkansdk_workarea}" ] ; then
+		mkdir "${vulkansdk_workarea}"
+	fi
+
+	pushd "${vulkansdk_workarea}"
+
+	install_runtime_dependencies
 
 	download_sdk_tar_package
 	verify_sdk_tar_package
 
-	vulkansdk_local="$(tar --list --file ${vulkansdk_tar} \
+	vulkansdk_extracted="$(tar --list --file ${vulkansdk_tar} \
 		| head -1 \
 		| sed -e 's|/$||')"
 
@@ -80,20 +130,21 @@ main() {
 		exit 1
 	fi
 
-	# TODO
-	# Experiencing the following:
-	# mv: cannot move 'vulkansdk-linux-x86_64-1.3.261.1' to '/home/tank/my/tools/vulkansdk-linux-x86_64-1.3.261.1/vulkansdk-linux-x86_64-1.3.261.1': Directory not empty
-	#
-	# Remove the destination directory or figure out something else.
-	echo "What might be happening here?"
-	mv --verbose "${vulkansdk_local}" "${vulkansdk_local_renamed}"
+	echo "Giving a more descriptive name to extracted SDK directory"
+	mv --verbose "${vulkansdk_extracted}" "${vulkansdk_extracted_renamed}"
+
+	echo "Moving SDK directory under destination directory"
 	mv --verbose \
-		"${vulkansdk_local_renamed}" \
+		"${vulkansdk_extracted_renamed}" \
 		"${vulkansdk_destination}/${vulkansdk_local_renamed}"
 
-	# TODO continue here
-
 	popd # vulkan
+
+	# TODO
+	# * [ ] Slackware runtime dependencies install
+	# * [ ] Check if there are more steps to SDK install
+	# * [ ] Finalize the ending echo
+	echo "[?] Finished Vulkan SDK install"
 }
 
 main "${@}"
